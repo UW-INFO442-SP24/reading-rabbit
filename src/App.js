72682489b2425f852432510237548c6b254694a7
jplopez19 <jlopez19@uw.edu>
2024-04-24 13:22:50 -0700

    code

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import About from './components/About/about.js';
+import Home from './components/Home/home.js';
+import Events from './components/Events/event.js';
+import Map from './components/Map/map.js';
+import Resources from './components/Resources/resources.js';
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+
+export default function App(props) {
+    return (
+      <div>
+        <Router>
+          <Routes>
+          <Route path="/Home" element={<Home/>} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Events" element={<Events/>} />
+          <Route path="/Map" element={<Map/>} />
+          <Route path="/Resources" element={<Resources/>} />
+          </Routes>
+        </Router> 
+      </div>
+    );
+  }
+  
---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,1 @@
+<li><a className="navigator" href="/About">About</a></li>
